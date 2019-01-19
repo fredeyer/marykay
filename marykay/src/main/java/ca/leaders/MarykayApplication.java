494731940a302f812135ea6170954980e98b0875
frederic <luckerf@yahoo.com>
2019-01-19 14:58:44 +0100

    first commit: just create projet
    Change-Id: I1a7a6cdee0a998029258177bc206bf05b038749c
    Signed-off-by: frederic <luckerf@yahoo.com>

---
+++ marykay/src/main/java/ca/leaders/MarykayApplication.java
@@ -0,0 +1,14 @@
+package ca.leaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarykayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarykayApplication.class, args);
+	}
+
+}
+
